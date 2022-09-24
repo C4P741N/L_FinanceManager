e67a9ff9a554a08862afd9c08327f25eef347b07
Dancan Mwanthi <dancanmwanthi@cim-pool.ch>
2022-09-24 13:15:27 +0300

    Addition of JS and a 404 on the Privacy page

--- MSota/wwwroot/js/site.js
+++ MSota/wwwroot/js/site.js
@@ -2,3 +2,59 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var spent = setInterval(AmountSpent, 10)
+var received = setInterval(AmountReceived, 10)
+var borrow = setInterval(AmountBorrowed, 1)
+var charged = setInterval(AmountCharged, 1)
+
+
+
+    let count1 = 1;
+    let count2 = 1;
+    let count3 = 1;
+    let count4 = 1;
+
+function AmountSpent() {
+
+    count1++
+
+    document.querySelector("#number1").innerHTML = count1
+
+    if (count1 == item.AmountSpent) {
+        clearInterval(spent)
+    }
+}
+
+function AmountReceived() {
+
+    count2++
+
+    document.querySelector("#number2").innerHTML = count2
+
+    if (count2 == item.AmountReceived) {
+        clearInterval(received)
+    }
+}
+
+function AmountBorrowed() {
+
+    count3++
+
+    document.querySelector("#number3").innerHTML = count3
+
+    if (count3 == item.AmountBorrowed) {
+        clearInterval(borrow)
+    }
+}
+
+function AmountCharged() {
+
+    count4++
+
+    document.querySelector("#number4").innerHTML = count4
+
+    if (count3 == item.AmountCharged) {
+        clearInterval(charged)
+    }
+}
