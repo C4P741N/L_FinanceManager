4dc73d173d059d8ed591fd094da91f6b06ffa1d5
Dancan Mwanthi <dancanmwanthi@cim-pool.ch>
2023-01-10 20:04:17 +0300

    Linked UI to Business Logic

--- MSota/Controllers/ButtonClick.cs
+++ MSota/Controllers/ButtonClick.cs
@@ -1,13 +1,28 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc;
 using BaseCommands;
 using MSota.Models;
 using DataAndStatistics;
 using LogicObjects;
+using System.Web.Http;
+using System.Net.Http;
+//using Azure.Core;
+//using Azure;
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Azure.Core;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Newtonsoft.Json.Linq;
+using System.Net.Http.Formatting;
+using System.Globalization;
 
 namespace MSota.Controllers
 {
-    public class ButtonClick : Controller
+    //[Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [System.Web.Http.Route("api/[controller]")]
+    [ApiController]
+
+    public class ButtonClick : ApiController
     {
         private DataAndStatistics.DataAndStatisticsProp dsprop = null;
         private Statement st = null;
@@ -15,29 +30,83 @@ namespace MSota.Controllers
         //private B_BaseCommands bc = null;
         private U_StatisticsProp uisprop = null;
         private L_Recepients xrps = null;
-        public IActionResult ExtractAndAddDataOnClick()
-        {
-            //bc = new B_BaseCommands();
 
-            //bc.BeginLaunchOfXMLStuff();
-            B_BaseCommands.BeginDataInsertIf();
 
-            return View();
-        }
+        //public HttpResponse ExtractAndAddDataOnClick()
+        //{
+        //    //bc = new B_BaseCommands();
+
+        //    //bc.BeginLaunchOfXMLStuff();
+        //    B_BaseCommands.BeginDataInsertIf();
+
+        //     return null;
+        //}
+
+        [System.Web.Http.HttpGet]
+        //public IActionResult RetriveDataOnClick()
+        //{
+        //    //bc = new B_BaseCommands();
+        //    st = new Statement();
+        //    stctr = new StatementController();
+
+        //    B_BaseCommands.BeginLaunchOfStuffToGetData(ref xrps);
+        //    //bc.BeginLaunchOfStuffToGetData(ref uisprop);
+
+        //    //stctr.Dispose();
+        //    stctr.StatementDetails(ref uisprop);
+
+        //    return View("/Views/Statement/StatementDetails.cshtml");
+        //}
+
+
         public IActionResult RetriveDataOnClick()
         {
-            //bc = new B_BaseCommands();
-            st = new Statement();
-            stctr = new StatementController();
+            List<int> numbers = new List<int>()
+            {
+                4,
+                6,
+                8
+            };
 
-            B_BaseCommands.BeginLaunchOfStuffToGetData(ref xrps);
-            //bc.BeginLaunchOfStuffToGetData(ref uisprop);
-
-            //stctr.Dispose();
-            stctr.StatementDetails(ref uisprop);
-
-            return View("/Views/Statement/StatementDetails.cshtml");
+            return (IActionResult)Ok(numbers);
         }
+
+        //[System.Web.Http.AcceptVerbs("GET")]
+        //public HttpResponseMessage RetriveDataOnClick()
+        //{
+           
+        //    st = new Statement()
+        //    {
+        //        AmountSpent= 0,
+        //        FulizaAmount=200,
+        //        FulizaBorrowed= 100,
+        //    };
+
+        //    List<int> numbers = new List<int>()
+        //    {
+        //        4,
+        //        6,
+        //        8
+        //    };
+
+        //    //HttpRequestMessage request;
+
+        //    //HttpRequestMessage test;
+        //    //test.Content;
+
+        //    HttpResponseMessage httpResponse = Request.CreateResponse<List<int>>(HttpStatusCode.Accepted,
+        //                                                               numbers);
+
+        //    return httpResponse;
+
+
+        //    //return new HttpResponseMessage()
+        //    //{
+        //    //    Content = new ObjectContent(st,
+        //    //      Configuration.Formatters.JsonFormatter)
+        //    //};
+
+        //}
 
     }
 }
