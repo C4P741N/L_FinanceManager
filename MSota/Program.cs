2fcaf1b5d6511e3e235bbd285585d43e862571d3
Dancan Mwanthi <dancanmwanthi@cim-pool.ch>
2023-01-27 19:29:01 +0300

    Fixed Cores error on server side

--- MSota/Program.cs
+++ MSota/Program.cs
@@ -8,12 +8,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
+
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseCors(option =>
+{
+    option.AllowAnyOrigin();
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
