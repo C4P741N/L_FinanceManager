086983684344c9df6e656d803316861f3478b504
Dancan Mwanthi <danconawm@gmail.com>
2023-03-14 00:16:10 +0300

    .gitignore is now working

--- MSota/BaseFormaters/BaseCommands.cs
+++ MSota/BaseFormaters/BaseCommands.cs
@@ -1,34 +1,9 @@
-﻿//using MSota.DataLibrary;
-//using MSota.Models;
-//using System.Transactions;
+﻿//using System.Text.RegularExpressions;
 
-//namespace MSota.Base
-//{
-//    public class BaseCommands
-//    {
-//        ITransactionsResponse _transactions;
-//        BaseCommands _baseCommands;
+//Select
+//     [Ms_Recepients].[M_RecepientName]      AS RecepientName
+//    , SUM([Ms_Transactions].M_CashAmount)  AS Amount
+//  FROM [DB_MSota].[dbo].[Ms_Recepients]
+//JOIN[DB_MSota].[dbo].[Ms_Transactions] ON[Ms_Recepients].[M_RecepientPhoneNo] = [Ms_Transactions].[M_RecepientPhoneNo]
 
-
-//        public BaseCommands(ITransactionsResponse transactions)
-//        {
-//            _transactions = transactions;
-//            //_baseCommands = baseCommands;
-//        }
-
-//        public static void BeginDataInsertIf()
-//        {
-//        }
-
-//        public ITransactionsResponse Transactions()
-//        {
-//            //BaseCommands baseCommands = new BaseCommands(_transactions);
-//            //_baseCommands = new BaseCommands(_transactions);
-//            //TransactionsResponse transactions = new TransactionsResponse();
-
-//            _transactions.CollectTransactions();
-
-//            return null;
-//        }
-//    }
-//}
+//  GROUP BY[Ms_Recepients].[M_RecepientName]
