670eb6aef1fdd239b67a67787b9ab8bb7eadcb9a
Dancan Mwanthi <dancanmwanthi@cim-pool.ch>
2023-08-22 18:07:17 +0300

    New table modifications

---
+++ Ms_DataCollector/dbo/Tables/Ms_Collection_II.sql
@@ -0,0 +1,18 @@
+﻿CREATE TABLE Ms_Collection_II
+(
+		DocNum				INT NOT NULL IDENTITY (1, 1),
+        DocEntry			VARCHAR(255) NOT NULL,
+		TranId			    VARCHAR(255) NULL,
+	    LongDate			    INT NOT NULL,
+        DocDateTime		    DATETIME NOT NULL,
+		Recepient		    VARCHAR(255) NOT NULL,
+        AccNo	            varchar(255),
+		TranAmount	        FLOAT(53) NOT NULL,
+		Balance			    FLOAT(53) NOT NULL,
+        Charges		        FLOAT(53) NOT NULL,
+        DocType		        VARCHAR(255),
+        Service_center	    VARCHAR(255),
+        IsRead				INT,
+        Quota				VARCHAR(255) NOT NULL,
+        Body				varchar(MAX) NOT NULL
+);
